e2f3aa211d5e05f165cc868523285bf07972d095
AntonioAlgarin <antonio@cybermindedvets.com>
2022-01-17 23:53:04 -0800

    assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,14 +15,13 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
-
 describe("codeWord", () => {
+    const secretCodeWord1 = "Lackadaisical"
+    // Expected output: "L4ck4d41s1c4l"
+    const secretCodeWord2 = "Gobbledygook"
+    // Expected output: "G0bbl3dyg00k"
+    const secretCodeWord3 = "Eccentric"
+    // Expected output: "3cc3ntr1c"
     it("takes a string, returns a coded message. 'a' becomes 4, 'e' becomes 3, 'i' becomes 1, and 'o' becomes 0.", () => {
       expect(codeWord(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
       expect(codeWord(secretCodeWord2)).toEqual("G0bbl3dyg00k")
@@ -48,14 +47,15 @@ const codeWord = (string) => {
 
 // a) Create a test with expects statement using the variable provided.
 
-const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-const letterA = "a"
-// Expected output: ["Apple", "Banana", "Orange"]
-const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
-const letterE = "e"
+
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 describe("wordArray", () => {
+    const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+    const letterA = "a"
+    // Expected output: ["Apple", "Banana", "Orange"]
+    const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+    const letterE = "e"
     it("takes in an array of words and a single letter and returns all the words that contain that particular letter", () => {
       expect(wordArray(arrayOfWords1, letterA)).toEqual(["Apple", "Banana", "Orange"])
       expect(wordArray(arrayOfWords2, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
